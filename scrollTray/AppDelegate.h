724de3dd7a828ff5ade45f76685a27039675863a
Tyler Miller <tylermiller@Tylers-MacBook-Pro.local>
2014-07-01 10:04:42 -0700

    Initial Commit

---
+++ scrollTray/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  scrollTray
+//
+//  Created by Tyler Miller on 7/1/14.
+//  Copyright (c) 2014 Tyler Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ scrollTrayTests/scrollTrayTests.m
@@ -0,0 +1,34 @@
+//
+//  scrollTrayTests.m
+//  scrollTrayTests
+//
+//  Created by Tyler Miller on 7/1/14.
+//  Copyright (c) 2014 Tyler Miller. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface scrollTrayTests : XCTestCase
+
+@end
+
+@implementation scrollTrayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
